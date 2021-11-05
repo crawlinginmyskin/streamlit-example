63d35dee7e06e7c4faa72de8c40e4d21b1c0df85
filip <fzietara2137@gmail.com>
2021-11-05 18:17:50 +0100

    attempt at fixing time display on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ byla_jazda = False
 
 x_train, x_test, y_train, y_test, time_slices, df_t, df_avg = obrobka_df('godziny.csv')
 test = [j.split()[1].split(':')[:-1] for i, j in enumerate(df_t) if j.split()[0] == data_str]
-test_t = tuple(time(int(test[i][0]), int(test[i][1])) for i in range(len(test)))
+test_t = [time(int(test[i][0]), int(test[i][1])) for i in range(len(test))]
 for i, j in enumerate(df_t):
 	if j.split()[0] == data_str:
 		byla_jazda = True
@@ -95,7 +95,7 @@ if byla_jazda and choice is not None:
 			
 			ilosc = time_slices[0][4] - time_slices[0][3] + 1
 			test =(j.split()[1] for i, j in enumerate(df_t) if j.split()[0] == data_str)
-			n = st.slider('Wybierz obserwację do analizy', test_t[0], test_t[-1], test_t[0], step=timedelta(minutes=60))
+			n = st.slider('Wybierz obserwację do analizy', time(test_t[0].hour), time(test_t[-1].hour), time(test_t[0].hour), step=timedelta(minutes=60))
 			st.write("tego dnia odczyty nastąpiły w tych godzinach: ")
 			for i in test_t:
 				st.write(i)
