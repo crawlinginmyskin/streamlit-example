162498ec56db1d544796482afd734b48124bd11c
filip <fzietara2137@gmail.com>
2021-11-05 18:10:05 +0100

    improved wording

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import datetime as dt
 import streamlit as st
 from funkcje import obrobka_df, load_models, podsumowanie, wykres_y, wykres_x, wykres_avg, rysuj_shapy, rysuj_waterfall, rysuj_barplot
 from PIL import Image
-
+from datetime import time, timedelta
 st.set_page_config(layout='wide')
 """
 # Regresja
@@ -24,7 +24,7 @@ col3.image(gait,use_column_width=True)
 data = st.date_input('Wybierz dzień do obserwacji', value=dt.date(2021, 9, 1), min_value=dt.date(2021, 9, 1),
 	                    max_value=dt.date(2021,9,30))
 
-choice = st.radio('wyświetl dane z: ', ('regresji liniowej', 'random forrest'))
+choice = st.radio('wybierz model: ', ('regresja liniowa', 'random forrest'))
 
 is_rf = False
 
@@ -47,6 +47,8 @@ else:
 byla_jazda = False
 
 x_train, x_test, y_train, y_test, time_slices, df_t, df_avg = obrobka_df('godziny.csv')
+test = [j.split()[1].split(':')[:-1] for i, j in enumerate(df_t) if j.split()[0] == data_str]
+test_t = tuple(time(int(test[i][0]), int(test[i][1])) for i in range(len(test)))
 for i, j in enumerate(df_t):
 	if j.split()[0] == data_str:
 		byla_jazda = True
@@ -55,20 +57,20 @@ for i, j in enumerate(df_t):
 if byla_jazda and choice is not None:
 	regs = load_models(is_rf)
 	preds = [i.predict(x_test) for i in regs]
-	with st.expander("Wykresy Y"):
+	with st.expander("Wyniki predykcji"):
 		"""
 		Przewidywana różnica między temperaturą w łożysku przednim/tylnym, a średnią z łożysk środkowych w danym dniu września
 		na podstawie modelu wytrenowanego na danych z miesięcy kwiecień-sierpień. Bezpieczny przedział to +- 3°C, a przedział ostrzegawczy to +- 2°C
 		"""
 		wykres_y(y_test, preds, data_str, df_t)
 
-	with st.expander("Wykresy X"):
+	with st.expander("Parametry wejściowe"):
 		"""
 		Przebieg zmiennych na podstawie których model dokonuje estymacji
 		"""
 		wykres_x(x_test, df_t, data_str)
 	
-	with st.expander("Wykres średniej"):
+	with st.expander("Temperatury z wózków tocznych BL1-CR2"):
 		"""
 		Przebieg średniej temperatury z wózków BL1-CR2, od której to różnicę przewidujemy naszym modelem
 		"""
@@ -81,19 +83,32 @@ if byla_jazda and choice is not None:
 		"""
 		st.write(podsumowanie(x_test, y_test, regs))
 	else:
-		with st.expander("Wykresy dla całego zbioru testowego"):
+		with st.expander("Wpływ parametrów wejsciowych na cały model"):
 			"""
 			Jak bardzo poszczególne dane wejściowe mają wpływ na ogół modelu
 			"""
 			rysuj_barplot(x_test, y_test, regs)
-		with st.expander("Wykresy dla poszczególnych obserwacji"):
+		with st.expander("Wpływ parametrów wejsciowych na pojedyncze predykcje"):
 			"""
 			Jak bardzo poszczególne dane wejściowe mają wpływ na pojedyncza decyzje
 			"""
 			
 			ilosc = time_slices[0][4] - time_slices[0][3] + 1
-			n = st.slider('wybierz obserwację do analizy', 1, ilosc, 1)
-			rysuj_waterfall(x_test, y_test, time_slices, regs, data_str,  n)
+			test =(j.split()[1] for i, j in enumerate(df_t) if j.split()[0] == data_str)
+			n = st.slider('Wybierz obserwację do analizy', test_t[0], test_t[-1], test_t[0], step=timedelta(minutes=60))
+			st.write("tego dnia odczyty nastąpiły w tych godzinach: ")
+			for i in test_t:
+				st.write(i)
+			if n in test_t:
+				n_index = 0
+				for i in range(len(test_t)):
+					if n == test_t[i]:
+						n_index = i
+				rysuj_waterfall(x_test, y_test, time_slices, regs, data_str,  n_index)
+			else:
+				st.write("brak odczytu o wybranej godzinie")
+				st.write(n)
+				
 		rysuj_shapy(y_test, x_test, regs)
 	st.write("metodologia opracowania modelu [link](https://docs.google.com/presentation/d/19SkSF6WnEuGmVQNwRAMcyPjQeVMGxQ3nIgM2y5akBgA/edit?usp=sharing)")
 	
